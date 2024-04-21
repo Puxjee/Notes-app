b4e3b9cb6db746138340941ec20188605d58a5df
Puxjee <melekelmokhtar@gmail.com>
2024-04-21 17:53:43 +0100

    Added functionalities to the page
    Added functions to allow store data in notes array, show empty page when array is empty, show new note page when the plus button is clicked, verifying the input fields, saves a new note to the array and display the notes page when array is not empty

--- main.js
+++ main.js
@@ -1,40 +1,103 @@
 const newNoteEl = document.querySelector('.new-note')
-const newNoteBtn = document.getElementById("add")
-
+const notesEmpty = document.querySelector('.empty')
 const notesEl = document.querySelector('.not-empty')
 
-const notesEmpty = document.querySelector('.empty')
+let notes = []
 
-const notes = []
 
-if (notes.length === 0 ){
-    notesEl.style.display = 'none'
-    notesEmpty.style.display = 'flex'
+
+function showNotes(){
+    if (notes !== null && notes.length < 1) {
+        notesEl.style.display = 'none';
+        notesEmpty.style.display = 'flex';
+        newNoteEl.style.display = 'none';
+    } else {
+        newNoteEl.style.display = 'none';
+        notesEl.style.display = 'block';
+        notesEmpty.style.display = 'none';
+
+        const activeNote = notesEl.querySelector('.active-note');
+        if (activeNote) {
+        activeNote.classList.remove('active-note');
+    }
+    }
 }
 
-newNoteBtn.addEventListener('click', () => {
-    newNoteEl.style.display = 'block'
-    notesEl.style.display = 'none'
-    notesEmpty.style.display = 'none'
+function showNewNoteForm() {
+    newNoteEl.style.display = 'block';
+    notesEl.style.display = 'none';
+    notesEmpty.style.display = 'none';
+}
+
+const newNoteBtn = document.getElementById("add")
+newNoteBtn.addEventListener("click", () => {
+    showNewNoteForm()
 })
 
-const backBtn = document.getElementById("back")
+function showNoteContent(noteTitle){
+    const noteContent = notes.find(note => note.title === noteTitle).content
+    let h2 = document.createElement('h2')
+    h2.classList.add("note-title")
+    h2.textContent = noteTitle
+    let p = document.createElement('p')
+    p.classList.add("note-content")
+    p.textContent = noteContent
+}
+
+function createNoteElement() {
+    notesEl.innerHTML = ''
+    notes.forEach(note => {
+        let newNote = document.createElement('div')
+        let h2 = document.createElement('h2')
+        newNote.classList.add('note')
+        h2.classList.add('note-title')
+        h2.textContent = note.title
+          
+        newNote.appendChild(h2)
+        notesEl.appendChild(newNote)
+    });
+}
+
+
+const backBtn = document.querySelector(".back")
 
 backBtn.addEventListener("click", () => {
-    newNoteEl.style.display = 'none'
-    notesEl.style.display = 'none'
-    notesEmpty.style.display = 'flex'
+    showNotes()
 })
 
-const saveBtn = document.getElementById("save")
+const saveBtn = document.querySelector(".save")
 
-saveBtn.addEventListener("click", () => {
-    const title = document.getElementById("title").value
-    const content = document.getElementById("content").value
+function saveNote() {
+    let titleInput = document.getElementById("title")
+    let contentInput = document.getElementById("content")
+    const title = titleInput.value.trim()
+    const content = contentInput.value.trim()
 
-    notes.push({title, content})
-    console.log(notes)
-    newNoteEl.style.display = 'none'
-    notesEl.style.display = 'block'
-    notesEmpty.style.display = 'none'
-})
+    if (!title) {
+        titleInput.style.borderBottom = '1px solid #ff9494'
+        contentInput.style.border = ''
+        return
+    } else if (!content) {
+        contentInput.style.borderBottom = '1px solid #ff9494'
+        titleInput.style.border = ''
+        return
+    } else {
+        titleInput.style.border = ''
+        contentInput.style.border = ''
+        notes.push({ title: title, content: content })
+
+        createNoteElement()
+        showNotes()
+        titleInput.value = ''
+        contentInput.value = ''
+    }
+
+    
+}
+
+saveBtn.addEventListener("click", saveNote)
+
+
+showNotes()
+
+const searchBtn = document.getElementById("search")
